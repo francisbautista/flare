9cd24daa2d01ce02229045de7e7a5a5a45768854
Francis Bautista <francis.bautista07@gmail.com>
2016-04-27 16:15:19 +0800

    Modify flare for venv support.

--- flare/flare.py
+++ flare/flare.py
@@ -6,19 +6,41 @@ __version__ = "0.0.1"
 import os
 import sys
 import logging
+import subprocess
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def main():
+    argument = sys.argv[1]
+    path = sys.argv[2]
+    switch(argument, path)
+
+def switch(argument, path):
+    options = {
+    "new" : new,
+    "start": start,
+    "stop": stop,
+    "remove": remove,
+    "--help": docs
+    }
+    if argument in options:
+        if argument == "new":
+            return options[argument](path)
+        else:
+            return options[argument]()
+    else:
+        error()
+
 def new(path):
     root_path = "{}/{}".format(os.getcwd(), path)
     if not os.path.exists(root_path):
+        create(root_path)
         logging.info('Creating %s', root_path)
         top_level_files = ["LICENSE", "README.md", "requirements.txt"]
         top_level_directories = ["data", "docs", "models", "notebooks", "ref", "reports", "src"]
         data_directories = ["raw", "temp", "prod"]
         src_directories = ["features", "models"]
-        os.mkdir(root_path)
         for f in top_level_files:
             open(f, 'a').close()
         for directory in top_level_directories:
@@ -30,23 +52,33 @@ def new(path):
     else:
         logging.info('Directory %s already exists', path)
 
-def error(): logging.info('Error on input')
+def error():
+    logging.info('Input is not recognized. Try --help to see documentation.')
 
-def main():
-    argument = sys.argv[1]
-    path = sys.argv[2]
-    switch(argument, path)
+def update():
+    subprocess.call(["pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | \
+    xargs -n1 pip install -U"], shell=True)
 
-def switch(argument, path):
-    options = {
-    "new" : new}
-    opt = options.get(argument, None)
+def docs():
+    print("""
+    Run "flare new <project-name> to create a new Flare project with a venv"
+    Run "flare start to start the venv"
+    Run "flare stop to stop the venv"
+    Run "flare update to update the Pip packages within the venv"
+    Run "flare remove to remove the venv"
+    """)
 
-    if opt != None:
-        return opt(path)
-    else:
-        error()
+def create(path):
+    subprocess.call(["virtualenv {}/venv".format(path)], shell=True)
 
+def remove():
+    subprocess.call(["rm -rf venv"], shell=True)
+
+def start():
+    subprocess.call(["source venv/bin/activate"], shell=True)
+
+def stop():
+    subprocess.call(["deactivate"], shell=True)
 
 if __name__ == '__main__':
     main()
